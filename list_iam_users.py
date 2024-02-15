0e45c2545b440584e5490c350348205fd25fd9e2
Dareus <dareusm@outlook.com>
2024-02-14 23:15:04 -0500

    List users in IAM

---
+++ list_iam_users.py
@@ -0,0 +1,13 @@
+#List all IAM users
+import boto3
+
+aws_management_console = boto3.session.Session(profile_name="default")
+iam_resource = aws_management_console.resource("iam")
+iam_client = aws_management_console.client("iam")
+
+for users in iam_resource.users.all():
+    print(users.name)
+    
+for users in iam_client.list_users()['Users']:
+    print(users['UserName'])
+    
